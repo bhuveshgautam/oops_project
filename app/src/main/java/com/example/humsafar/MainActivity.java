e8cb097c6cc4b5e8432b45b6562eedf16a355515
Gaurav Sharma <gauravsharma10167@gmail.com>
2019-10-07 23:05:52 +0530

    Second commit

--- app/src/main/java/com/example/humsafar/MainActivity.java
+++ app/src/main/java/com/example/humsafar/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.humsafar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addData(){
+
+    }
+
+    public void getData(){
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        long phoneno = intent.getLongExtra("phoneno", 0);
+        String ans = intent.getStringExtra("address");
     }
 }
---
+++ app/src/main/java/com/example/humsafar/Models/personalinfo.java
@@ -0,0 +1,13 @@
+package com.example.humsafar.Models;
+
+public class personalinfo {
+    protected String name;
+    protected long phoneno;
+    protected String address;
+
+    personalinfo(String name, long phoneno, String address){
+        this.name = name;
+        this.phoneno = phoneno;
+        this.address = address;
+    }
+}
